359a9732faf5305b7ccd3f4d5f6eff5adce23b9e
jnubel <jnubel@posteo.ch>
2020-05-13 18:14:41 +0200

    Code compacted: only one searchSpotifyID function

--- Code/search_spotify/search_spotify.py
+++ Code/search_spotify/search_spotify.py
@@ -2,25 +2,37 @@
 
 import spotify_connect as sc
 
-def artistSearchId(artist):
+def searchSpotifyID(query,type='artist',owner='Spotify'):
+      """"
+      Search for a Spotify object that match a query, artist by default.
+      Could be playlist, a track, an album...
       """
-      Searching for an artist, return a dictionary of spotify ids which match with the query
-      """
-      spot = sc.spotifyPublic()
-      artist = spot.search(artist, limit=50, offset=0, type='artist', market=sc.marketall)
-      artist_ids = {}
-      for art in artist['artists']['items']:
-            artist_ids[art['id']] = art['name']
-      return artist_ids
 
-def playlistSearchId(playlist, owner='Spotify'):
-      """
-      Searching for a playlist from a particular owner or owned by Spotify (default), return a dictionary of spotify ids which match with the query
-      """
       spot = sc.spotifyPublic()
-      playlist = spot.search(playlist, limit=50, offset=0, type='playlist', market=sc.marketall)
-      playlists_id = {}
-      for play in playlist['playlists']['items']:
-            if play['owner']['display_name'] == owner:
+
+      if type == 'artist':
+            """
+            Searching for an artist. 
+            Return a dictionary of spotify ids which match with the query
+            """
+
+            artist = spot.search(query, limit=50, offset=0, type='artist', market=sc.marketall)
+            artist_ids = {}
+            for art in artist['artists']['items']:
+                  artist_ids[art['id']] = art['name']
+            return artist_ids
+    
+      if type == 'playlist':
+            """
+            Searching for a playlist from a particular owner or owned by Spotify (default).
+            Return a dictionary of spotify ids which match with the query
+            """
+
+            playlist = spot.search(query, limit=50, offset=0, type='playlist', market=sc.marketall)
+            playlists_id = {}
+            for play in playlist['playlists']['items']:
+                  if play['owner']['display_name'] == owner:
                         playlists_id[play['id']] = play['name']
-      return playlists_id
+            return playlists_id
+      else:
+            pass
